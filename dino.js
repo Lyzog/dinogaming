8a29d20666970047791644a221a7e690fa1a22f4
Avecca <hanna_astlind@hotmail.com>
2018-11-05 14:32:09 +0100

    Dynamic conent based on menu choices

--- dino.js
+++ dino.js
@@ -1,5 +1,4 @@
 
-
 /* Scrolling down below the menu makes the menu stick to the
 top and adds a link at the bottom that takes you to the top*/
 window.onscroll = function() {stickyNav()};
@@ -15,5 +14,40 @@ function stickyNav() {
       list.classList.remove("sticky");
       title.classList.remove("visible-title");
     }
+
+}
+
+var menu = document.getElementsByClassName("menu");
+var selectedMenu = document.getElementById("rpg");
+var selectedContent = document.getElementsByClassName("rpg");
+
+//Make the menubar clickable
+for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", menuClicked);
+}
+
+//when a menuItem is clicked
+function menuClicked() {
+
+  //ta bort befintliga meny val
+  selectedMenu.classList.remove("highlight");
+  //highlighta det nya valet
+  this.classList.add("highlight");
+  selectedMenu = this;
+
+  //Ta bort befintligt valda spel
+  for (let i = 0; i < selectedContent.length; i++) {
+    console.log("test");
+    selectedContent[i].classList.remove("visible_game");
+  }
+
+  //Hitta nya valda spel
+  selectedContent = document.getElementsByClassName(selectedMenu.id);
+  console.log(selectedContent);
+  //Visa nya valda spel
+  for (var i = 0; i < selectedContent.length; i++) {
+    selectedContent[i].classList.add("visible_game");
+  }
+
 
 }
