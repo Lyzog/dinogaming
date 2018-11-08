fd95eb103b85107f53c86816e3097ab8ffb1b28f
Avecca <hanna_astlind@hotmail.com>
2018-11-08 20:50:06 +0100

    Fun times where had

--- dino.js
+++ dino.js
@@ -8,7 +8,7 @@ var sticky = list.offsetTop;
 var title = document.getElementById("title");
 
 function stickyNav() {
- /*
+/*
      if (window.pageYOffset < sticky ){ //136 140
       list.classList.remove("sticky");
       title.classList.remove("visible-title");
@@ -25,6 +25,7 @@ function stickyNav() {
       list.classList.remove("sticky");
       title.classList.remove("visible-title");
     }
+
 
 }
 
@@ -135,3 +136,54 @@ document.getElementById("myFrame").onload = function() {pageLoad()};
 window.onload = function() {
   console.log("MOBILE!!");
 };*/
+
+
+
+/* Vid kontakt klick- gör allt lila*/
+// Hämta contact texten
+var selectContact = document.getElementById("blah");
+//Make the contacttext clickable
+selectContact.addEventListener("click", contactClick);
+
+//hämta elementen där vi vill ändra
+var selectMenuHover = document.getElementsByClassName("menu");
+var selectGameBg = document.getElementsByClassName("gamecontent_icon");
+var bgOrig = true;
+var bgPurple = "purple";
+var bgBlack = "black";
+var bgBlue = "rgb(191, 220, 230)";
+var bgGray = "rgb(139, 139, 138)";
+
+function contactClick() {
+
+   if (!bgOrig) {   //"radial-gradient(rgb(191, 220, 230), rgb(139, 139, 138))";
+     document.body.style.background = "radial-gradient("+bgBlue+", "+bgGray+ ")";
+     document.getElementsByTagName("header")[0].style.background  = bgBlue;
+     document.getElementsByTagName("nav")[0].style.background  = bgGray;
+     document.getElementsByTagName("footer")[0].style.background  = bgBlue;
+
+     for (var i = 0; i < selectMenuHover.length; i++) {
+       selectMenuHover[i].classList.remove("purple_menu");
+     }
+     for (var i = 0; i < selectGameBg.length; i++) {
+       selectGameBg[i].classList.remove("purple_gamecontent");
+     }
+     bgOrig = true;
+   }
+   else {
+    document.body.style.background = "radial-gradient("+bgPurple+", "+bgBlack+")";
+    document.getElementsByTagName("header")[0].style.background  = bgPurple;
+    document.getElementsByTagName("nav")[0].style.background  = bgBlack;
+    document.getElementsByTagName("footer")[0].style.background  = bgPurple;
+
+    //gör alla hovers i menyn lila
+    for (var i = 0; i < selectMenuHover.length; i++) {
+      selectMenuHover[i].classList.add("purple_menu");
+    }
+    //gör alla spel divar bg lila
+    for (var i = 0; i < selectGameBg.length; i++) {
+      selectGameBg[i].classList.add("purple_gamecontent");
+    }
+    bgOrig = false;
+  }
+}
